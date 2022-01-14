18592c6e0ec4e93ffee73bc81d1ca666cac6f238
Ighoorr <i.bereshchak@gmail.com>
2022-01-14 18:23:20 +0200

    update

--- Task 18/sorting/sorting/Program.cs
+++ Task 18/sorting/sorting/Program.cs
@@ -10,8 +10,17 @@ namespace sorting
 
 class Program
 {
-    static void Main(string[] args)
-    {
+
+        private static int ComparerInt(object o1, object o2)
+        {
+            return ((int)o1).CompareTo((int)o2);
+        }
+        private static int ComparerString(object o1, object o2)
+        {
+            return ((string)o1).CompareTo((string)o2);
+        }
+        static void Main(string[] args)
+        {
         var array = new Product[3];
 
             array[0] = new Product() { Price = 3, Name = "BBB" };
@@ -20,11 +29,11 @@ class Program
      
 
        
-            SortClass.QuickSort<Product>(array, 0, array.Length - 1, array[0].GetType().GetProperty("Price"), false);//1 CCC ;2 AAA; 3 BBB
-            SortClass.QuickSort<Product>(array, 0, array.Length - 1, array[0].GetType().GetProperty("Name"), false);//1 AAA ; 2 BBB ; 3 CCC
-            SortClass.HeapSort<Product>(array, 0, array.Length - 1, array[0].GetType().GetProperty("Name"), true);//1 CCC; 2 BBB; 3 AAA
+            SortClass.QuickSort<Product>(array, ComparerInt, 0, array.Length - 1, array[0].GetType().GetProperty("Price"), false);//1 CCC ;2 AAA; 3 BBB
+            SortClass.QuickSort<Product>(array, ComparerString, 0, array.Length - 1, array[0].GetType().GetProperty("Name"), false);//1 AAA ; 2 BBB ; 3 CCC
+            SortClass.HeapSort<Product>(array, ComparerString, 0, array.Length - 1, array[0].GetType().GetProperty("Name"), true);//1 CCC; 2 BBB; 3 AAA
 
-         
-    }
+            
+        }
 }
 }
--- Task 18/sorting/sorting/SortClass.cs
+++ Task 18/sorting/sorting/SortClass.cs
@@ -11,71 +11,57 @@ namespace sorting
     class SortClass
     {
 
-            private static int ComparerInt(object o1, object o2)
-            {
-                return ((int)o1).CompareTo((int)o2);
-            }
-            private static int ComparerString(object o1, object o2)
-            {
-                return ((string)o1).CompareTo((string)o2);
-            }
+         
            
-            public static void QuickSort<T>(T[] array, int low, int high,System.Reflection.PropertyInfo propertyInfo, bool order)
+            public static void QuickSort<T>(T[] array,Comparer compare, int low, int high, System.Reflection.PropertyInfo propertyInfo, bool order)
             {
+                int p=0;
                 if (low < high)
                 {
-                    int p = Partition(array, low, high, propertyInfo, order);
-                    QuickSort<T>(array, low, p - 1, propertyInfo, order);
-                    QuickSort<T>(array, p + 1, high, propertyInfo, order);
-                }
-            }
+                    Comparer comparer = compare;
+                    T pivot = array[low];
+                     int i = high + 1;
 
-            static int Partition<T>(T[] array, int low, int high, System.Reflection.PropertyInfo propertyInfo, bool order)
-            {
-            
-                Comparer comparer = null;
-                T pivot = array[low];
-                int i = high + 1;
-
-                
-
-                if (propertyInfo.GetValue(pivot) is int)
-                    comparer = ComparerInt;
-                if (propertyInfo.GetValue(pivot) is string)
-                    comparer = ComparerString;
-
-
-                for (int j = high; j >= low + 1; j--)
-                {
-                    if (comparer(propertyInfo.GetValue(array[j]), propertyInfo.GetValue(pivot)) * ((order) ? -1 : 1) > 0)
+                    for (int j = high; j >= low + 1; j--)
                     {
-                        i--;
-                        Swap(array, i, j);
-                    }
+                        if (comparer(propertyInfo.GetValue(array[j]), propertyInfo.GetValue(pivot)) * ((order) ? -1 : 1) > 0)
+                        {
+                            i--;
+                            Swap(array, i, j);
+                         }
+                     }
+    
+                     Swap(array, i - 1, low);
+                     p= (i - 1);
+
+                 QuickSort<T>(array, compare, low, p - 1, propertyInfo, order);
+                 QuickSort<T>(array, compare, p + 1, high, propertyInfo, order);
+                   
+
                 }
 
-                Swap(array, i - 1, low);
-                return (i - 1);
+                    
             }
+
 
             private static void Swap<T>(T[] array, int i, int j)
             {
                 T temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+                array[i] = array[j];
+                 array[j] = temp;
             }
 
            
 
-            public static void HeapSort<T>(T[] array, int low, int high, System.Reflection.PropertyInfo propertyInfo, bool order)
+            public static void HeapSort<T>(T[] array, Comparer comparer, int low, int high, System.Reflection.PropertyInfo propertyInfo, bool order)
             {
                 int length = array.Length;
 
                 for (int i = (array.Length) / 2; i > 0; i--)
                 {
-                    int pos = BuildHeap<T>(array, i, length, propertyInfo, order);
+                    int pos = BuildHeap<T>(array, comparer, i, length, propertyInfo, order);
                     while (pos >= 0)
-                    pos = BuildHeap<T>(array, pos + 1, length, propertyInfo, order);
+                    pos = BuildHeap<T>(array, comparer, pos + 1, length, propertyInfo, order);
                 }
 
                 for (int i = array.Length; i > 1; i--)
@@ -84,79 +70,40 @@ namespace sorting
                 array[length - 1] = array[0];
                 array[0] = temp;
 
-                    int pos = BuildHeap<T>(array, 1, --length, propertyInfo, order);
+                    int pos = BuildHeap<T>(array, comparer, 1, --length, propertyInfo, order);
                     while (pos >= 0)
-                    pos = BuildHeap<T>(array, pos + 1, length, propertyInfo, order);
+                    pos = BuildHeap<T>(array, comparer, pos + 1, length, propertyInfo, order);
                 }
             }
 
-            private static int BuildHeap<T>(T[] array, int fatherNumber, int bound, System.Reflection.PropertyInfo propertyInfo, bool order)
+            private static int BuildHeap<T>(T[] array, Comparer compare, int fatherNumber, int bound, System.Reflection.PropertyInfo propertyInfo, bool order)
             {
                 int fatherIndex = fatherNumber - 1;
                 int leftSonIndex = (fatherNumber * 2 - 1 < array.Length && fatherNumber * 2 - 1 < bound) ? fatherNumber * 2 - 1 : -1;
                 int rightSonIndex = (fatherNumber * 2 < array.Length && fatherNumber * 2 < bound) ? fatherNumber * 2 : -1;
 
-              Comparer comparer = null;
+              Comparer comparer = compare;
 
 
-            if (propertyInfo.GetValue(array[0]) is int)
-                comparer = ComparerInt;
-            if (propertyInfo.GetValue(array[0]) is string)
-                comparer = ComparerString;
-
-            
-
-
-                if (order)
-                {
-                    if ((leftSonIndex < 0 && rightSonIndex < 0) ||
-                        (rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0) ||
-                        (comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0 &&
-                        comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[rightSonIndex])) <= 0))
+                
+                    if ((leftSonIndex < 0 && rightSonIndex < 0) || (rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0) || (comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0 && comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[rightSonIndex])) <= 0))
                         return -1;
-                    else if ((rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0) ||
-                        (comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0 &&
-                        comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[rightSonIndex])) <= 0))
+                    else if ((rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0) || (comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0 && comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[rightSonIndex])) <= 0))
                     {
                         T temp = array[fatherIndex];
                     array[fatherIndex] = array[leftSonIndex];
                     array[leftSonIndex] = temp;
                         return leftSonIndex;
                     }
-                    else if (comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0 &&
-                        comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0)
+                    else if (comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[fatherIndex])) <= 0 && comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[leftSonIndex])) <= 0)
                     {
                         T temp = array[fatherIndex];
                     array[fatherIndex] = array[rightSonIndex];
                     array[rightSonIndex] = temp;
                         return rightSonIndex;
                     }
-                }
-                else
-                {
-                    if ((leftSonIndex < 0 && rightSonIndex < 0) ||
-                        (rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) >= 0) ||
-                        (comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[leftSonIndex])) >= 0 &&
-                        comparer(propertyInfo.GetValue(array[fatherIndex]), propertyInfo.GetValue(array[rightSonIndex])) >= 0))
-                        return -1;
-                    else if ((rightSonIndex < 0 && comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) >= 0) ||
-                        (comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[fatherIndex])) >= 0 &&
-                        comparer(propertyInfo.GetValue(array[leftSonIndex]), propertyInfo.GetValue(array[rightSonIndex])) >= 0))
-                    {
-                        T temp = array[fatherIndex];
-                    array[fatherIndex] = array[leftSonIndex];
-                    array[leftSonIndex] = temp;
-                        return leftSonIndex;
-                    }
-                    else if (comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[fatherIndex])) >= 0 &&
-                        comparer(propertyInfo.GetValue(array[rightSonIndex]), propertyInfo.GetValue(array[leftSonIndex])) >= 0)
-                    {
-                        T temp = array[fatherIndex];
-                    array[fatherIndex] = array[rightSonIndex];
-                    array[rightSonIndex] = temp;
-                        return rightSonIndex;
-                    }
-                }
+                
+               
 
                 return -1;
             }
